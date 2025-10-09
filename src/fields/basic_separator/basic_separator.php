9bf20941ab2a14a0dca431a5f592752238de0713
Andre Wester <wester@mediamus.de>
2025-10-09 06:22:05 +0200

    Create the new fields.php to create the fields for this module and refactored the module to the new structure

--- src/fields/basic_separator/basic_separator.php
+++ src/fields/basic_separator/basic_separator.php
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 <?php echo $modules->get('WesanoxHelperClasses')->getSeparator($page);
+=======
+<?php echo $modules->get('WesanoxHelperClasses')->getSeparator($page->matrix('type'), $page);
+>>>>>>> c2f4cfe (Create the new fields.php to create the fields for this module and refactored the module to the new structure)
